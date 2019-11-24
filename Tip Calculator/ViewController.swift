46578b10868b933a350e162a98f1f604ffe7b383
Nada Zeini <nada.elzeini@sjsu.edu>
2019-11-23 20:56:07 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Nada Zeini on 11/23/19.
+//  Copyright © 2019 Nada Zeini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
